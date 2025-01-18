6fae17a95649d698e1d09e36d45656dd71f8943c
Andrei Shelest <shelest.andrej.12@gmail.com>
2025-01-18 01:10:56 +0100

    group2 param search (framework)

---
+++ group2_param_search.R
@@ -0,0 +1,62 @@
+source("group2_strats.R")
+
+library(quantmod)
+
+tickers_config = data.frame(
+  row.names=c("CAD", "AUD", "XAU", "XAG"),
+  transaction_cost = c(10, 10, 15, 10),
+  point_value = c(100000, 100000, 100, 5000)
+)
+
+plot_pnls = function(aggr_data, selected_quarter)
+{
+  myTheme <- chart_theme()
+  myTheme$col$line.col <- "darkblue"
+  
+  plot(cbind(cumsum(aggr_data$gross_pnl),
+             cumsum(aggr_data$net_pnl)),
+       multi.panel = FALSE,
+       main = paste0("Gross and net PnL for asset group 2 \n quarter ", selected_quarter), 
+       col = c("#377EB8", "#E41A1C"),
+       major.ticks = "weeks", 
+       grid.ticks.on = "weeks",
+       grid.ticks.lty = 3,
+       legend.loc = "topleft",
+       cex = 1)
+}
+
+load_quarter = function(selected_quarter)
+{
+  filename_ = paste0("data/data2_", selected_quarter, ".RData")
+  load(filename_)
+  
+  ticker_data = get(paste0("data2_", selected_quarter))
+  return(ticker_data)
+}
+
+# test utils
+# apply_strategy_test = function()
+# {
+Sys.setlocale("LC_TIME", "English")
+Sys.setenv(TZ = 'America/New_York')
+
+# prepare pos_flat
+
+selected_quarter = "2022_Q1"
+
+tickers_data = load_quarter(selected_quarter)
+
+pos_flat = init_pos_flat(tickers_data)
+pos_flat = apply_trading_time_assumptions(tickers_data, pos_flat)
+
+# apply test strategy
+
+# calculate EMA10, 60 for CAD
+ema_strategy = create_EMA(tickers_data, "CAD", tickers_config, 10, 60, pos_flat, 'mom')
+aggr_data = ema_strategy$get_daily_aggregates()
+strategy_metrics = get_strategy_metrics(aggr_data)
+
+plot_pnls(aggr_data, selected_quarter)
+
+
+# }
--- group2_utils.R
+++ group2_utils.R
@@ -1,10 +1,7 @@
 library(xts)
 library(chron)
 library(lubridate)
-library(quantmod)
-library(TTR)
-library(tseries)
-library(glue)
+
 
 init_pos_flat = function(quarter_data)
 {
@@ -94,87 +91,18 @@ target_metric = function(net_calmar_ratio, net_pnl)
   net_calmar_ratio * max(0, log(abs(net_pnl/1000)))
 }
 
-apply_EMA = function(quarter_data, ticker, fast, slow, pos_flat)
+get_strategy_metrics = function(daily_aggregates)
 {
-  ticker_data = quarter_data[, ticker]
-
-  ema_fast = EMA(na.locf(ticker_data), n=fast)
-  ema_slow = EMA(na.locf(ticker_data), n=slow)
+  strat_stats = list(
+    gross_sr = my_sharpe_ratio(x = daily_aggregates$gross_pnl, scale = 252),
+    net_sr = my_sharpe_ratio(x = daily_aggregates$net_pnl, scale = 252),
+    gross_calmar_ratio = my_calmar_ratio(x = daily_aggregates$gross_pnl, scale = 252),
+    net_calmar_ratio = my_calmar_ratio(x = daily_aggregates$net_pnl, scale = 252),
+    av_n_trades = mean(daily_aggregates$n_trans, na.rm = TRUE),
+    cum_gross_pnl = sum(daily_aggregates$gross_pnl),
+    cum_net_pnl = sum(daily_aggregates$net_pnl)
+  )
+  strat_stats$target_metric = target_metric(strat_stats$net_calmar_ratio, strat_stats$net_pnl)
   
-  ema_fast[is.na(ticker_data)] = NA
-  ema_slow[is.na(ticker_data)] = NA
-  
-  mom_strat = ifelse(lag.xts(ema_fast) > lag.xts(ema_slow), 1, -1)
-  mom_strat[pos_flat == 1] = 0
-  mom_strat = na.locf(mom_strat, na.rm = FALSE)
-  
-  return(mom_strat)
+  strat_stats
 }
-
-# test utils
-# apply_strategy_test = function()
-# {
-Sys.setlocale("LC_TIME", "English")
-Sys.setenv(TZ = 'America/New_York')
-
-# prepare pos_flat
-
-selected_quarter = "2022_Q1"
-filename_ = paste0("data/data2_", selected_quarter, ".RData")
-load(filename_)
-
-ticker_data = get(paste0("data2_", selected_quarter))
-
-pos_flat = init_pos_flat(ticker_data)
-pos_flat = apply_trading_time_assumptions(ticker_data, pos_flat)
-
-myTheme <- chart_theme()
-myTheme$col$line.col <- "darkblue"
-
-# apply test strategy
-
-# calculate EMA10, 60 for CAD
-mom_strat = apply_EMA(ticker_data, "CAD", 10, 60, pos_flat)
-
-# number of transactions
-n_trans = get_number_of_transactions(mom_strat)
-
-# gross pnl
-gross_pnl = get_gross_pnl(ticker_data, "CAD", mom_strat, 100000)
-
-# net pnl
-net_pnl = get_net_pnl(ticker_data, "CAD", gross_pnl, n_trans, 10)
-
-# daily data
-aggr_data = aggregate_daily(gross_pnl, net_pnl, n_trans)
-
-
-# stats
-
-strat_stats = list(
-  gross_sr = my_sharpe_ratio(x = aggr_data$gross_pnl, scale = 252),
-  net_sr = my_sharpe_ratio(x = aggr_data$net_pnl, scale = 252),
-  gross_calmar_ratio = my_calmar_ratio(x = aggr_data$gross_pnl, scale = 252),
-  net_calmar_ratio = my_calmar_ratio(x = aggr_data$net_pnl, scale = 252),
-  av_n_trades = mean(aggr_data$n_trans, na.rm = TRUE),
-  cum_gross_pnl = sum(aggr_data$gross_pnl),
-  cum_net_pnl = sum(aggr_data$net_pnl)
-)
-strat_stats$target_metric = target_metric(strat_stats$net_calmar_ratio, strat_stats$net_pnl)
-
-strat_stats
-
-# plot
-
-plot(cbind(cumsum(aggr_data$gross_pnl),
-           cumsum(aggr_data$net_pnl)),
-     multi.panel = FALSE,
-     main = paste0("Gross and net PnL for asset group 2 \n quarter ", selected_quarter), 
-     col = c("#377EB8", "#E41A1C"),
-     major.ticks = "weeks", 
-     grid.ticks.on = "weeks",
-     grid.ticks.lty = 3,
-     legend.loc = "topleft",
-     cex = 1)
-
-# }
